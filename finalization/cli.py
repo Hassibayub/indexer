9a5a3dbdbdf0a27ed24755230b3ef30d1bb798c2
Muhammad Haseeb <miltech.haseeb@gmail.com>
2024-03-19 04:01:06 +0500

    cmd print rm

--- finalization/cli.py
+++ finalization/cli.py
@@ -6,6 +6,4 @@ from finalization.install_with_py import install
 
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    
-    print('cmd: ', 'streamlit', 'run', os.path.join(dir_path, 'webapp.py'))
     subprocess.call(['streamlit', 'run', os.path.join(dir_path, 'webapp.py')] + sys.argv[1:])
